0d5c86fb1b20f588a8cd5460bb9349c4ac48f207
sripr <sripr@PRIYA>
2017-02-04 15:33:02 +0530

    Initial barebone application to take string input and give a response

---
+++ QueryBuilderAPI/QueryBuilderAPI/Controllers/NLPQuerySearchController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using QueryBuilderAPI.Models;
+
+namespace QueryBuilderAPI.Controllers
+{
+    public class NLPQuerySearchController : ApiController
+    {
+        [HttpPost]
+        public HttpResponseMessage Search(NLPQuerySearch searchIns)
+        {
+            //TODO: To allow users to save the query like JIRA query.
+            return new HttpResponseMessage() { StatusCode = HttpStatusCode.OK };
+        }
+    }
+}
---
+++ QueryBuilderAPI/QueryBuilderAPI/Models/NLPQuerySearch.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace QueryBuilderAPI.Models
+{
+    public class NLPQuerySearch
+    {
+        public string searchString { get; set; }
+    }
+}
